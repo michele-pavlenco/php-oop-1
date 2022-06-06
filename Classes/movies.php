3feb6e12cc87767e8b0a0278477e758cef5308dc
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-06-06 17:51:18 +0200

    end

--- Classes/movies.php
+++ Classes/movies.php
@@ -1,34 +1,16 @@
 <?php
-$movies = [
-        [
-            'titolo' => 'Interstellar',
-            'genere' => 'Sci-fi/Avventura',
-            'trama' => 'In un futuro non precisato, un drastico cambiamento climatico colpisce duramente l agricoltura. Il granturco è l unica coltivazione ancora in grado di crescere ed un gruppo di scienziati è intenzionato ad attraversare lo spazio per trovare nuovi luoghi adatti a coltivarlo.'
-        ],
-        [
-            'titolo' => 'Il cavaliere oscuro',
-            'genere' => 'Azione/Avventura',
-            'trama' => 'Batman e Jim Gordon si alleano con il nuovo procuratore di Gotham City, Harvey Dent, per combattere il crimine organizzato che dilaga in città e fermare un pericoloso rapinatore, il Joker, che ha gettato la città nell anarchia.'
-        ],
-        [
-            'titolo' => 'Tenet',
-            'genere' => 'Azione/Sci-fi',
-            'trama' => 'Un agente segreto riceve una sola parola come arma e viene inviato per prevenire l inizio della Terza Guerra Mondiale. Deve viaggiare nel tempo e piegare le leggi della natura per avere successo nella sua missione.'
-        ]
-    ];
-
 class Movie {
-    public $titolo ;
-    public $genere ;
-    public $trama ;
+    protected $titolo;
+    protected $genere;
+    protected $trama;
 
-    function __construct($_titolo, $genere, $trama){
-        $this->titolo = $_titolo;
-        $this->genere = $_genere;
-        $this->trama = $_trama ;  
+    function __construct($titolo, $genere, $trama){
+        $this->titolo = $titolo;
+        $this->genere = $genere;
+        $this->trama = $trama;  
      }
-     public function getTitle(){
-         return $this->title;
+     public function getTitolo(){
+         return $this->titolo;
      }
      public function getGenere(){
         return $this->genere;
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php 
 require_once __DIR__ . '/Classes/movies.php';
+$movies = [
+    [
+        'titolo' => 'Interstellar',
+        'genere' => 'Sci-fi/Avventura',
+        'trama' => 'In un futuro non precisato, un drastico cambiamento climatico colpisce duramente l agricoltura. Il granturco è l unica coltivazione ancora in grado di crescere ed un gruppo di scienziati è intenzionato ad attraversare lo spazio per trovare nuovi luoghi adatti a coltivarlo.'
+    ],
+    [
+        'titolo' => 'Il cavaliere oscuro',
+        'genere' => 'Azione/Avventura',
+        'trama' => 'Batman e Jim Gordon si alleano con il nuovo procuratore di Gotham City, Harvey Dent, per combattere il crimine organizzato che dilaga in città e fermare un pericoloso rapinatore, il Joker, che ha gettato la città nell anarchia.'
+    ],
+    [
+        'titolo' => 'Tenet',
+        'genere' => 'Azione/Sci-fi',
+        'trama' => 'Un agente segreto riceve una sola parola come arma e viene inviato per prevenire l inizio della Terza Guerra Mondiale. Deve viaggiare nel tempo e piegare le leggi della natura per avere successo nella sua missione.'
+    ]
+];
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,23 +32,28 @@ require_once __DIR__ . '/Classes/movies.php';
 <body>
     <main>
         <div class="container">
-            <?php foreach ($movies as $key => $value) {
-                        $movie = new Movie($value['titolo'], $value['trama']);
-                    ?>
-                    <h1 >
-                        Titolo:
-                        <?php echo $movie->getTitle(); ?>
+           <?php
+                foreach ($movies as $key => $value) { 
+                    $new_movie = new Movie($value['titolo'], $value['genere'], $value['trama']);   
+                     //echo $movie->getGenere()
+           ?>
+            <h1>
+                <?php echo $new_movie->getTitolo(); ?>
+            </h1>
+            <span>
+                <p style="color:red">
+                    Genere: 
+                </p>
+                <?php echo $new_movie->getGenere(); ?>
+            </span>
+            <span>
+                <p style="color:red">
+                    Trama: 
+                </p>
+                <?php echo $new_movie->getTrama(); ?>
+            </span>
 
-                    </h1>
-                    <p>
-                        Genere:
-                        <?php echo $movie->getGenere(); ?>
-                    </p>
-                    <p>
-                        Trama:
-                        <?php echo $movie->getTrama(); ?>
-                    </p>
-                    <?php } ?>
+<?php } ?>
 
         </div>
     </main>
